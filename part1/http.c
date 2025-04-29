3a851c6ed44c7638b1b9460cabddf57162b49c80
yabi <youssefabdulle12@gmail.com>
2025-04-29 05:07:03 +0000

    http.c done

--- part1/http.c
+++ part1/http.c
@@ -29,11 +29,89 @@ const char *get_mime_type(const char *file_extension) {
 }
 
 int read_http_request(int fd, char *resource_name) {
-    // TODO Not yet implemented
+    char buffer[BUFSIZE];
+    int total_bytes = 0;
+    int bytes_read;
+
+    while (1) {
+        bytes_read = read(fd, buffer + total_bytes, sizeof(buffer) - total_bytes - 1);
+        if (bytes_read < 0) {
+            perror("read");
+            return -1;
+        }
+        if (bytes_read == 0) {
+            break;
+        }
+        total_bytes += bytes_read;
+        buffer[total_bytes] = '\0';
+
+        if (strstr(buffer, "\r\n\r\n") != NULL) {
+            break;
+        }
+    }
+
+    char method[16];
+    char path[256];
+    char version[16];
+
+    if (sscanf(buffer, "%s %s %s", method, path, version) != 3) {
+        return -1;
+    }
+
+    if (strcmp(method, "GET") != 0) {
+        return -1;
+    }
+
+    strcpy(resource_name, path);
     return 0;
 }
 
 int write_http_response(int fd, const char *resource_path) {
-    // TODO Not yet implemented
+    struct stat st;
+    if (stat(resource_path, &st) != 0) {
+        const char *not_found_response = "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n";
+        write(fd, not_found_response, strlen(not_found_response));
+        return 0;
+    }
+
+    int file_fd = open(resource_path, O_RDONLY);
+    if (file_fd < 0) {
+        perror("open");
+        return -1;
+    }
+
+    const char *extension = strrchr(resource_path, '.');
+    const char *mime_type;
+    if (extension == NULL) {
+        mime_type = "application/octet-stream";
+    } else {
+        mime_type = get_mime_type(extension);
+    }
+
+    char header[BUFSIZE];
+    int header_len = snprintf(header, sizeof(header),
+                              "HTTP/1.0 200 OK\r\n"
+                              "Content-Type: %s\r\n"
+                              "Content-Length: %ld\r\n"
+                              "\r\n",
+                              mime_type, st.st_size);
+
+    if (write(fd, header, header_len) != header_len) {
+        perror("write header");
+        close(file_fd);
+        return -1;
+    }
+
+    char buffer[BUFSIZE];
+    int bytes;
+    while ((bytes = read(file_fd, buffer, sizeof(buffer))) > 0) {
+        if (write(fd, buffer, bytes) != bytes) {
+            perror("write body");
+            close(file_fd);
+            return -1;
+        }
+    }
+
+    close(file_fd);
     return 0;
 }
