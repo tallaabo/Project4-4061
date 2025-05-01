d629758310e274cbbbafd0ee6e778efd3d5bb783
Aman Has <atallaabo@gmail.com>
2025-05-01 05:31:18 +0000

    added implementation for main and http.c for part2

--- part2/http.c
+++ part2/http.c
@@ -30,10 +30,71 @@ const char *get_mime_type(const char *file_extension) {
 
 int read_http_request(int fd, char *resource_name) {
     // TODO Not yet implemented
+    char buf[BUFSIZE];
+    int total = 0, n;
+
+    while (1) {
+        n = read(fd, buf + total, sizeof(buf) - total - 1);
+        if (n < 0) { perror("read"); return -1; }
+        if (n == 0) break;
+        total += n;
+        buf[total] = '\0';
+        if (strstr(buf, "\r\n\r\n")) break;
+    }
+
+    char method[16], path[256], ver[16];
+    if (sscanf(buf, "%15s %255s %15s", method, path, ver) != 3) {
+        return -1;
+    }
+
+    if (strcmp(method, "GET") != 0) return -1;
+    strcpy(resource_name, path);
+
     return 0;
 }
 
 int write_http_response(int fd, const char *resource_path) {
     // TODO Not yet implemented
+    struct stat st;
+
+    if (stat(resource_path, &st) < 0) {
+        const char *resp = "HTTP/1.0 404 Not Found\r\nContent-Length: 0\r\n\r\n";
+        write(fd, resp, strlen(resp));
+        return 0;
+    }
+
+    int f = open(resource_path, O_RDONLY);
+    if (f < 0) {
+        perror("open"); return -1;
+    }
+
+    const char *ext = strrchr(resource_path, '.');
+    const char *mime = ext ? get_mime_type(ext) : "application/octet-stream";
+    char header[BUFSIZE];
+    int hlen = snprintf(header, sizeof(header),
+                        "HTTP/1.0 200 OK\r\n"
+                        "Content-Type: %s\r\n"
+                        "Content-Length: %ld\r\n"
+                        "\r\n",
+                        mime, st.st_size);
+
+    if (write(fd, header, hlen) != hlen) {
+        perror("write header");
+        close(f);
+        return -1;
+    }
+
+    char buf[BUFSIZE];
+    int r;
+    
+    while ((r = read(f, buf, sizeof(buf))) > 0) {
+        if (write(fd, buf, r) != r) {
+            perror("write body");
+            close(f);
+            return -1;
+        }
+    }
+    close(f);
+
     return 0;
 }
--- part2/http_server.c
+++ part2/http_server.c
@@ -20,11 +20,25 @@
 
 int keep_going = 1;
 const char *serve_dir;
+static connection_queue_t queue;
 
 void handle_sigint(int signo) {
     keep_going = 0;
 }
 
+void *worker_thread(void *_) {
+    while (1) {
+        int conn = connection_queue_dequeue(&queue);
+        if (conn < 0) break;  // shutdown
+        char resource[BUFSIZE], fullpath[BUFSIZE*2];
+        if (read_http_request(conn, resource) == 0) {
+            snprintf(fullpath, sizeof(fullpath), "%s%s", serve_dir, resource);
+            write_http_response(conn, fullpath);
+        }
+        close(conn);
+    }
+    return NULL;
+}
 
 int main(int argc, char **argv) {
     // First argument is directory to serve, second is port
@@ -33,9 +47,70 @@ int main(int argc, char **argv) {
         return 1;
     }
     // Uncomment the lines below to use these definitions:
-    // serve_dir = argv[1];
-    // const char *port = argv[2];
+    serve_dir = argv[1];
+    const char *port = argv[2];
 
     // TODO Implement the rest of this function
+
+    // 1) Install SIGINT handler
+    struct sigaction sa = { .sa_handler = handle_sigint };
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGINT, &sa, NULL);
+
+    // 2) Block all signals in this thread BEFORE creating workers
+    sigset_t full, prev;
+    sigfillset(&full);
+    sigprocmask(SIG_SETMASK, &full, &prev);
+
+    // 3) Init queue and spawn worker threads
+    if (connection_queue_init(&queue) != 0) {
+        perror("queue init");
+        return 1;
+    }
+    pthread_t threads[N_THREADS];
+    for (int i = 0; i < N_THREADS; i++) {
+        pthread_create(&threads[i], NULL, worker_thread, NULL);
+    }
+
+    // 4) Restore signal mask so main thread catches SIGINT
+    sigprocmask(SIG_SETMASK, &prev, NULL);
+
+    // 5) Setup listening socket
+    struct addrinfo hints = { .ai_family = AF_INET,
+                              .ai_socktype = SOCK_STREAM,
+                              .ai_flags = AI_PASSIVE }, *res;
+    int server_fd;
+    if (getaddrinfo(NULL, port, &hints, &res) != 0 ||
+        (server_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol)) < 0 ||
+        bind(server_fd, res->ai_addr, res->ai_addrlen) < 0 ||
+        listen(server_fd, LISTEN_QUEUE_LEN) < 0) {
+        perror("setup server");
+        return 1;
+    }
+    freeaddrinfo(res);
+
+    // 6) Main accept loop
+    while (keep_going) {
+        int conn = accept(server_fd, NULL, NULL);
+        if (conn < 0) {
+            if (errno == EINTR) break;
+            perror("accept");
+            continue;
+        }
+        if (connection_queue_enqueue(&queue, conn) < 0) {
+            close(conn);
+            break;
+        }
+    }
+
+    // 7) Shutdown
+    close(server_fd);
+    connection_queue_shutdown(&queue);
+    for (int i = 0; i < N_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    connection_queue_free(&queue);
+
     return 0;
 }
